635183718b86070f8c929d695669fe934b15d765
KurokawaAyane <ayane@KurokawaAyanes-MacBook-Air.local>
2022-05-31 15:01:56 -0600

    Add update

--- Setting.js
+++ Setting.js
@@ -4,9 +4,12 @@ import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
-function Setting() {
+function Setting(props) {
     return(
       <View>
+      <text>
+        Welcome {props.loggedInUser}
+      </text>
       <Button
       title="Log In"
       onPress={() => Alert.alert('Simple Button pressed')}
